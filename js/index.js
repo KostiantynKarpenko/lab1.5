1028f555591e07b1ff430d6ae9acaf09fac93ae3
KostiantynKarpenko <10252215@stud.op.edu.ua>
2023-06-06 09:51:10 +0300

    modified:   css/index.css 	modified:   css/media.css 	new file:   img/eureka.jpg 	modified:   js/index.js 	modified:   less/index.less 	deleted:    less/media.css 	modified:   less/media.less

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 const $pageSliderBox = document.querySelector('.page-content .testimonials .container .page-slider .box')
+const $menuButton = document.querySelector('.mobile.menu-button')
+const $menu = document.querySelector('.menu-popup')
 
-function tgl() {
-    $pageSliderBox.classList.toggle('active');
-}
+// function tgl() {
+//     $pageSliderBox.classList.toggle('active');
+// }
 
-$pageSliderBox.addEventListener('click', tgl);
+// $pageSliderBox.addEventListener('click', tgl);
+
+$menuButton.addEventListener('click', function(e) {
+    $menu.classList.toggle('active');
+})
